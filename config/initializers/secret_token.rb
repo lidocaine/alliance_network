707a5f8c899324c60fcc9e5644c8f4a1d308cb02
MJ Watkins <c14h22n2o@gmail.com>
2011-01-28 12:17:58 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllianceNetwork::Application.config.secret_token = '516c391610810ba6853d3aee866a659e7df3260bdd39bffa8c7e21556346d7e1d661dd66a5062df42dd3513b8839b086360bc358eed701e6f1f8add651a5bb33'
