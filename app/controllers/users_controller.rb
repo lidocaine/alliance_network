fb113e4dfa2f37c0465ea457efba648392aa8e0b
MJ Watkins <c14h22n2o@gmail.com>
2011-03-01 12:08:01 -0500

    Finished basic user registration (open).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,22 @@ class UsersController < ApplicationController
     @title = "Sign up"
     @user = User.new
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Alliance Network!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      # Clear submitted password fields and render the sign-up form.
+      @user.password = ""
+      @user.password_confirmation = ""
+      render 'new'
+    end
+    
+  rescue ActiveRecord::StatementInvalid
+    redirect_to root_path
+  end
 
 end
